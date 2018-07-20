75726268843524ccaa956e36d8bf8c2ccc768617
lixiaojun <echemoo@gmail.com>
2018-07-20 12:06:09 +0800

    2018-07-20

--- MiniServ/daily.sh
+++ MiniServ/daily.sh
@@ -1,5 +1,5 @@
 cd /home/echemoo/github/servermonitor/MiniServ
-find /home -iname "*" -atime 1 -type f > `date +"%Y-%m-%d"`
+find / -iname "*" -atime 1 -type f > `date +"%Y-%m-%d"`
 git add -A
 git commit -m `date +"%Y-%m-%d"`
 git push
